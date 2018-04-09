c64d1ed640233b324216ed61737d76cd00972a96
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-04-09 14:26:21 -0300

    Add questao_5

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -1,19 +1,31 @@
 ## QUESTÃO 5 ##
-# Escreva um programa que leia as medidas dos lados de um triângulo e escreva se ele é 
+# Escreva um programa que leia as medidas dos lados de um triângulo e escreva se ele é
 # Equilátero, Isósceles ou Escaleno.
 ##
 
 
 ##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
+# A sua resposta da questão deve ser desenvolvida dentro da função main()!!!
 # Deve-se substituir o comado print existente pelo código da solução.
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
+
 def main():
-    print("questao 5")
+    triangle_sides = [float(side) for(side) in input(
+        "Type the sizes of each side spaced between: ").split()]
+
+    if(triangle_sides[0] == triangle_sides[1]):
+        if(triangle_sides[1] == triangle_sides[2]):
+            print("Your triangle is Equilateral")
+            return
+    elif(triangle_sides[0] != triangle_sides[2]):
+        if(triangle_sides[1] != triangle_sides[2]):
+            print("Your triangle is Scalene")
+            return
+
+    print("Your triangle is Isosceles")
 
 
-    
 if __name__ == '__main__':
     main()
