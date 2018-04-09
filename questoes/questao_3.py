1272f927693db5a9566f120b57e4ea53e4f17440
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-04-09 11:33:22 -0300

    Add questao_3

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -1,22 +1,30 @@
 ## QUESTÃO 3 ##
-# Implementar um programa que calcula o desconto previdenciário de um funcionário. 
-# O programa deve, dado um salário, retornar o valor do desconto proporcional ao mesmo. 
-# O cálculo de desconto segue a regra: o desconto deve 11% do valor do salário, entretanto, 
+# Implementar um programa que calcula o desconto previdenciário de um funcionário.
+# O programa deve, dado um salário, retornar o valor do desconto proporcional ao mesmo.
+# O cálculo de desconto segue a regra: o desconto deve 11% do valor do salário, entretanto,
 # o valor máximo de desconto é 318,20.
 # Sendo assim, ou o programa retorna 11% sobre o salário ou 318,20.
 ##
 
 
 ##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
+# A sua resposta da questão deve ser desenvolvida dentro da função main()!!!
 # Deve-se substituir o comado print existente pelo código da solução.
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
+
+def _get_discount(salary):
+    return 0.11*salary
+
+
 def main():
-    print("questao 3")
+    salary = float(input("Type your salary: "))
+    discount = _get_discount(salary)
+    if(discount > 318.20):
+        discount = 318.20
+    print("You got a discount of", discount, "$")
 
 
-    
 if __name__ == '__main__':
     main()
