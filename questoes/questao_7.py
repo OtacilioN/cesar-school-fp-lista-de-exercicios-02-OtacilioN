d69b134276b3579d7032477b8cb10c115a25bfdc
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-04-10 10:01:02 -0300

    Add questao_7

--- questoes/questao_7.py
+++ questoes/questao_7.py
@@ -1,27 +1,35 @@
 ## QUESTÃO 7 ##
-# A maioria dos anos tem 365 dias. No entanto, o tempo necessário para a Terra orbitar 
-# o Sol é na verdade um pouco mais que isso. Como resultado, um dia extra, 29 de fevereiro, 
-# está incluído em alguns anos para corrigir essa diferença. Esses anos são referidos como 
+# A maioria dos anos tem 365 dias. No entanto, o tempo necessário para a Terra orbitar
+# o Sol é na verdade um pouco mais que isso. Como resultado, um dia extra, 29 de fevereiro,
+# está incluído em alguns anos para corrigir essa diferença. Esses anos são referidos como
 # anos bissextos. As regras para determinar se um ano é ou não um ano bissexto seguem:
 # • Qualquer ano que seja divisível por 400 é um ano bissexto.
 # • Dos anos restantes, qualquer ano que seja divisível por 100 não é um ano bissexto.
 # • Dos anos restantes, qualquer ano que seja divisível por 4 é um ano bissexto.
 # • Todos os outros anos não são anos bissextos.
-# Escreva um programa que leia um ano do usuário e exiba uma mensagem indicando 
+# Escreva um programa que leia um ano do usuário e exiba uma mensagem indicando
 # se é ou não um ano bissexto.
 ##
 
 
 ##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
+# A sua resposta da questão deve ser desenvolvida dentro da função main()!!!
 # Deve-se substituir o comado print existente pelo código da solução.
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
+def _is_multiple(number, multiple):
+    return not bool(number % multiple)
+
+
 def main():
-    print("questao 7")
+    year = int(input("Type the year to discover if is a leap year: "))
+    if _is_multiple(year, 400) or (_is_multiple(year, 4) and not _is_multiple(year, 100)):
+        print("The year", year, "is a leap year")
+        return
+
+    print("The year", year, "is NOT a leap year")
 
 
-    
 if __name__ == '__main__':
     main()
